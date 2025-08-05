f642a494b7229bc85ea108570a3b8e60779ab929
ImJake11 <jakejuguilon843@gmail.com>
2025-08-05 16:14:07 +0800

    add suspense

--- app/ui/auth/confirm-email/page.tsx
+++ app/ui/auth/confirm-email/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ConfirmationBody from './ConfirmationBody'
 
 const page = () => {
 
     return (
         <div className='w-screen h-screen bg-linear-120 from-black to-[#0C2340] grid place-content-center'>
-            <ConfirmationBody />
+            <Suspense>
+                <ConfirmationBody />
+            </Suspense>
         </div>
     )
 }
