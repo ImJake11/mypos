6d7c0b2a8f1cf5f7c423dc87334e75e99047d0b0
ImJake11 <jakejuguilon843@gmail.com>
2025-08-05 17:04:39 +0800

    fix

--- app/ui/notifications-page/page.tsx
+++ app/ui/notifications-page/page.tsx
@@ -1,31 +1,29 @@
 import GlobalWrapper from '@/app/lib/components/GlobalWrapper'
-import Sidebar from '@/app/lib/components/Sidebar/Sidebar'
 import React from 'react'
 import NotificationPageBody from './NotificationBody'
 import NotificationPageAppbar from './components/NotificationPageAppbar'
 import NotificationPageStatus from './components/NotificationPageStatus'
 import { NotificationModel } from '@/app/lib/models/notificationModel'
 import { NotificationFilterType } from '@/app/lib/enum/notificationType'
+import { prisma } from '@/app/lib/utils/db/prisma'
 
 
 const page = async () => {
 
-    const protocol = process.env.WEBSITE_PROTOCOL;
-    const host = process.env.WEBSITE_HOST;
-    const port = process.env.WEBSITE_PORT;
+    const now = new Date();
 
-    const baseUrl = `${protocol}://${host}:${port}`;
+    const start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+    const end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
 
-    let data: NotificationModel[] = [];
+    const notifications = await prisma.notifications.findMany({
+        where: {
+            createdAt: {
+                gte: start,
+                lte: end,
+            }
+        },
+    })
 
-
-    const res = await fetch(`${baseUrl}/api/notification?filter=${NotificationFilterType.TODAY}&&limit=10`);
-
-    if (!res.ok) {
-        return <div></div>
-    }
-
-    const { notifications } = await res.json();
 
     const child = (
         <div className='w-full h-full flex flex-col overflow-hidden'>
